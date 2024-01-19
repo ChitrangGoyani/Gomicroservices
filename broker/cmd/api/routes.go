9e11191b54006238e8d110660a84c631ccb0e584
ChitrangGoyani <chitranggoyani@gmail.com>
2024-01-19 11:09:56 -0500

    started building broker microservice

---
+++ broker/cmd/api/routes.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+// pointer receiver - https://chat.openai.com/share/1b5117cc-69fd-45eb-a135-56d05e1db938
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// specify who is allowed to connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping")) // make sure service is still alive
+	mux.Post("/", app.Broker)
+	return mux
+}
